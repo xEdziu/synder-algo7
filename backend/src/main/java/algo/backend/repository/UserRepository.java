a8dc22c600e74e3fb128f46216d0e35c5ab89da0
Mateusz Andrzejewski <161636237+MANDRW@users.noreply.github.com>
2025-11-08 15:53:32 +0100

    feat: add UserRepository interface for user data access

---
+++ backend/src/main/java/algo/backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package algo.backend.repository;
+
+import algo.backend.model.entity.User;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Transactional
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
+
