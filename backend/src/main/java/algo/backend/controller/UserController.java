ef1673d4b11dea375c96516033289532d4d908bf
Aiffelowy <adas.krzywicki2@gmail.com>
2025-11-09 05:13:00 +0100

    :(

--- backend/src/main/java/algo/backend/controller/UserController.java
+++ backend/src/main/java/algo/backend/controller/UserController.java
@@ -2,20 +2,19 @@ package algo.backend.controller;
 
 import algo.backend.model.dto.UserDTO;
 import algo.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/authorized")
 public class UserController {
-    private final UserService userService;
+  private final UserService userService;
 
-    @GetMapping("/user")
-    public UserDTO getAuthorizedUser() {
-        return userService.getAuthorizedUser();
-    }
+  @GetMapping("/user")
+  public UserDTO getAuthorizedUser() {
+    return userService.getAuthorizedUser();
+  }
 }
