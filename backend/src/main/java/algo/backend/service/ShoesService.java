73919badcca3f8f23a2224110326d9047dd6dfa5
Mateusz Andrzejewski <161636237+MANDRW@users.noreply.github.com>
2025-11-09 05:35:29 +0100

    feat: update getAllShoes method to return a response map with ShoesDTO list

--- backend/src/main/java/algo/backend/service/ShoesService.java
+++ backend/src/main/java/algo/backend/service/ShoesService.java
@@ -1,18 +1,32 @@
 package algo.backend.service;
 
+import algo.backend.config.CustomReturnables;
+import algo.backend.model.dto.ShoesDTO;
 import algo.backend.model.entity.Shoes;
 import algo.backend.repository.ShoesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class ShoesService {
     private final ShoesRepository shoesRepository;
 
-    public List<Shoes> getAllShoes() {
-        return shoesRepository.findAll();
+    public Map<String, Object> getAllShoes() {
+        List<ShoesDTO> shoesList = shoesRepository.findAll().stream()
+                .map(shoes -> new ShoesDTO(
+                        shoes.getId(),
+                        shoes.getShoesType().toString(),
+                        shoes.getSize(),
+                        shoes.getPrice(),
+                        shoes.getProductionPrice()
+                )).toList();
+
+        Map<String, Object> response = CustomReturnables.getOkResponseMap("SHOES WAS DOWNLOADED SUCCESSFULLY");
+        response.put("shoes", shoesList);
+        return response;
     }
 }
